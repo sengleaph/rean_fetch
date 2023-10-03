b7db8ccbedfab7b2b0d1c2b444e16c57cb435c33
Seng-Leaph <sengleaphseang@gmail.com>
2023-10-03 13:49:56 +0700

    fetch api

---
+++ lib/views/home/widgets/gridfood.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class GridFood extends StatelessWidget {
+  const GridFood({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 130,
+          decoration:BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.grey[100]
+          ),
+          child: Image.network('https://bobakingusa.com/wp-content/uploads/2020/09/oolongmilktea-1.png'),
+        ),
+        const Text("Milk Tea",
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              height: 2
+          ),)
+      ],
+    );
+  }
+}
